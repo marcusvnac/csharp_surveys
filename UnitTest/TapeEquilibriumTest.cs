af3b378b4ead14ddad211323639fcc4ca26b5c72
marcusvnac <marcusvnac@gmail.com>
2016-09-29 19:40:34 -0300

    First Version

---
+++ UnitTest/TapeEquilibriumTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TapeEquilibriumTest
+    {
+        [TestMethod]
+        public void TapeEquilibriumTestMethod1()
+        {
+            int[] A = { 3, 1, 2, 4, 3 };
+
+            Assert.AreEqual(TapeEquilibrium.Solution(A), 1);
+        }
+    }
+}
