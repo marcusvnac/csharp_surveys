3a3ffc19222a6e8e7e3f432c90952927ff27e037
marcusvnac <marcusvnac@gmail.com>
2016-09-29 20:43:06 -0300

    PermCheck

---
+++ UnitTest/PermCheckUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class PermCheckUnitTest
+    {
+        [TestMethod]
+        public void PermCheckUnitTestMethod1()
+        {
+            int[] A = { 4, 1, 3, 2 };
+
+            Assert.AreEqual(PermCheck.Solution(A), 1);
+        }
+
+        [TestMethod]
+        public void PermCheckUnitTestMethod2()
+        {
+            int[] A = { 4, 1, 3 };
+
+            Assert.AreEqual(PermCheck.Solution(A), 0);
+        }
+
+    }
+}
