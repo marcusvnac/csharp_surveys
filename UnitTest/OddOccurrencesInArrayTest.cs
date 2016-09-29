9f5cc86457034e99326a4cdb2ee0f2d08c767dc5
marcusvnac <marcusvnac@gmail.com>
2016-09-29 19:17:08 -0300

    First Version

---
+++ UnitTest/OddOccurrencesInArrayTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class OddOccurrencesInArrayTest
+    {
+        [TestMethod]
+        public void SolutionTest1()
+        {
+            int[] A = { 9, 3, 9, 3, 9, 7, 9 };
+
+            Assert.AreEqual(OddOccurrencesInArray.Solution(A), 7);                
+        }
+    }
+}
