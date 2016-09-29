4e2d16c8af03dfa5180053f4537d36e7c46f26fe
marcusvnac <marcusvnac@gmail.com>
2016-09-29 19:58:49 -0300

    FrogJmp

---
+++ CSharpSurveys/FrogJmp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSurveys
+{
+    public static class FrogJmp
+    {
+        public static int Solution(int X, int Y, int D)
+        {
+            double aux = Math.Ceiling((Y - X) / (double)D);
+
+            int result = Convert.ToInt32(Math.Ceiling(aux));
+
+            return result;
+        }
+    }
+}
---
+++ UnitTest/FrogJmpUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class FrogJmpUnitTest
+    {
+        [TestMethod]
+        public void FrogJmpUnitTestMethod1()
+        {
+            Assert.AreEqual(FrogJmp.Solution(10, 85, 30), 3);
+        }
+    }
+}
