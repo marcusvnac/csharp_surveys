f7d732687d90b5da3205f2979a0edaf894473c7f
marcusvnac <marcusvnac@gmail.com>
2016-09-30 19:55:49 -0300

    PassingCars

---
+++ CSharpSurveys/PassingCars.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSurveys
+{
+    public static class PassingCars
+    {
+        public static int Solution(int[] A)
+        {
+            int count = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 0)
+                {
+                    for (int j = i + 1; j < A.Length; j++)
+                    {
+                        if (A[j] == 1)
+                            count++;
+                    }
+                }
+            }
+
+            if (count > 1000000000)
+                return -1;
+            else
+                return count;
+        }
+    }
+}
