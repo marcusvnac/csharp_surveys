80d0a32e0c53355367dd5181c3e901144eed3aac
marcusvnac <marcusvnac@gmail.com>
2016-09-30 20:09:17 -0300

    CountDiv

---
+++ CSharpSurveys/CountDiv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSurveys
+{
+    public static class CountDiv
+    {
+        public static int Solution(int A, int B, int K)
+        {
+            int count = 0;
+            for (int i = A; i <= B; i++)
+            {
+                if (i % K == 0)
+                {
+                    count++;
+                    i++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ UnitTest/CountDivUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class CountDivUnitTest
+    {
+        [TestMethod]
+        public void CountDivUnitTestMethod1()
+        {
+            int A = 6, B = 11, K = 2;
+
+            Assert.AreEqual(CountDiv.Solution(A, B, K), 3);
+        }
+    }
+}
