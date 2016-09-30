86725566fd75f5e6ae03e06db98b913b9f029204
marcusvnac <marcusvnac@gmail.com>
2016-09-29 21:36:07 -0300

    FrogRiverOne

---
+++ UnitTest/FrogRiverOneUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class FrogRiverOneUnitTest
+    {
+        [TestMethod]
+        public void FrogRiverOneUnitTestMethod1()
+        {
+            int[] A = { 1, 3, 1, 4, 2, 3, 5, 4 };
+
+            Assert.AreEqual(FrogRiverOne.Solution(5, A), 6);
+        }
+    }
+}
