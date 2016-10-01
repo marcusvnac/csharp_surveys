4b0ada8502fd1cf1585b00a6c227714ad83f39a7
marcusvnac <marcusvnac@gmail.com>
2016-10-01 10:47:25 -0300

    MaxProductOfThree

---
+++ CSharpSurveys/MaxProductOfThree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSurveys
+{
+    public static class MaxProductOfThree
+    {
+        public static int Solution(int[] A)
+        {
+            if (A.Length >= 3)
+            {
+                Array.Sort(A);
+
+                int res = A[A.Length - 3] * A[A.Length - 2] * A[A.Length - 1];
+
+                return res;
+            }
+            else
+                return 0;
+        }
+    }
+}
---
+++ UnitTest/MaxProductOfThreeUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class MaxProductOfThreeUnitTest
+    {
+        [TestMethod]
+        public void MaxProductOfThreeTestMethod1()
+        {
+            int[] A = { -3, 1, 2, -2, 5, 6 };
+
+            Assert.AreEqual(60, MaxProductOfThree.Solution(A));
+        }
+
+        [TestMethod]
+        public void MaxProductOfThreeTestMethod2()
+        {
+            int[] A = { 0, -1, -2, -3, -4, -5 };
+
+            Assert.AreEqual(0, MaxProductOfThree.Solution(A));
+        }
+
+        [TestMethod]
+        public void MaxProductOfThreeTestMethod3()
+        {
+            int[] A = { 3, 3, 3, 3, 3, 3 };
+
+            Assert.AreEqual(27, MaxProductOfThree.Solution(A));
+        }
+
+        [TestMethod]
+        public void MaxProductOfThreeTestMethod4()
+        {
+            int[] A = { 1000, 1000, 1000, 1000, 1000, 1000 };
+
+            Assert.AreEqual(1000000000, MaxProductOfThree.Solution(A));
+        }
+
+        [TestMethod]
+        public void MaxProductOfThreeTestMethod5()
+        {
+            int[] A = { 1000, 1000, -1000, -1000, -1000, -1000 };
+
+            Assert.AreEqual(-1000000000, MaxProductOfThree.Solution(A));
+        }
+    }
+}
