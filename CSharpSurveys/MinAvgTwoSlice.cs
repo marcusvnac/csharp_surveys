baf092dcaa80e94fbdb44d7a2c915dc348726659
marcusvnac <marcusvnac@gmail.com>
2016-10-01 10:04:33 -0300

    MinAvgTwoSlice

---
+++ CSharpSurveys/MinAvgTwoSlice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSurveys
+{
+    public static class MinAvgTwoSlice
+    {
+        public static int Solution(int[] A)
+        {
+            double[] avgArr = new double[A.Length];
+            double minAvg = double.MaxValue;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                avgArr[i] = int.MaxValue;
+
+                var sum = A[i];
+                for (int j = i+1; j < A.Length; j++)
+                {
+                    sum += A[j];
+                    var avg = sum / (double)(j - i + 1);
+                    if (avg < avgArr[i])
+                        avgArr[i] = avg;
+                }
+
+                if (avgArr[i] < minAvg)
+                    minAvg = avgArr[i];
+            }
+
+            int res = 0;
+            for (int i = avgArr.Length-1; i >=0 ; i--)
+            {
+                if (avgArr[i].Equals(minAvg))
+                    res = i;
+
+            }
+            return res;
+        }
+    }
+}
---
+++ UnitTest/MinAvgTwoSliceUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class MinAvgTwoSliceUnitTest
+    {
+        [TestMethod]
+        public void MinAvgTwoSliceTestMethod1()
+        {
+            int[] A = { 4, 2, 2, 5, 1, 5, 8 };
+
+            Assert.AreEqual(1, MinAvgTwoSlice.Solution(A));
+        }
+    }
+}
