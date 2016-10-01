ff9698f834511236359be3158690fca04bc61fa4
marcusvnac <marcusvnac@gmail.com>
2016-10-01 16:18:07 -0300

    MaxSliceSum

---
+++ CSharpSurveys/Codility/MaxSliceSum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSurveys.Codility
+{
+    public static class MaxSliceSum
+    {
+        public static int Solution(int[] A)
+        {
+            int max_ending = 0;
+            int max_slice = int.MinValue;
+
+            foreach (var a in A)
+            {
+                int sum;
+
+                if ((max_ending + a) > int.MaxValue)
+                    sum = int.MaxValue;
+                else
+                    sum = max_ending + a;
+
+                max_ending = Math.Max(a, sum);
+                max_slice = Math.Max(max_slice, max_ending);
+            }
+
+            return max_slice;
+        }
+    }
+}
---
+++ UnitTest/Codility/MaxSliceSumUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys.Codility;
+
+namespace UnitTest.Codility
+{
+    [TestClass]
+    public class MaxSliceSumUnitTest
+    {
+        [TestMethod]
+        public void MaxSliceSumTestMethod1()
+        {
+            int[] A = { 3, 2, -6, 4, 0 };
+
+            Assert.AreEqual(5, MaxSliceSum.Solution(A));
+        }
+
+        [TestMethod]
+        public void MaxSliceSumTestMethod2()
+        {
+            int[] A = { 3 };
+
+            Assert.AreEqual(3, MaxSliceSum.Solution(A));
+        }
+
+
+        [TestMethod]
+        public void MaxSliceSumTestMethod3()
+        {
+            int[] A = { -10, -20, -5 };
+
+            Assert.AreEqual(-5, MaxSliceSum.Solution(A));
+        }
+    }
+}
