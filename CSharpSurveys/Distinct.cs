b9a5c60c76f70b730ca7d08d7db4d4e8ed27ca23
marcusvnac <marcusvnac@gmail.com>
2016-10-01 11:00:24 -0300

    Distinct

---
+++ CSharpSurveys/Distinct.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSurveys
+{
+    public static class Distinct
+    {
+        public static int Solution(int[] A)
+        {
+            Array.Sort(A);
+
+            if (A.Length > 0)
+            {
+                int counter = 1;
+                int priorNumber = A[0];
+
+                for (int i = 1; i < A.Length; i++)
+                {
+                    if (A[i] != priorNumber)
+                    {
+                        counter++;
+                        priorNumber = A[i];
+                    }
+                }
+                return counter;
+            }
+            else
+                return 0;
+        }
+    }
+}
