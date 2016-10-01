d75a43c110fc184c0243095d8841f7cbf04a4867
marcusvnac <marcusvnac@gmail.com>
2016-10-01 14:56:43 -0300

    StoneWallUnit

---
+++ CSharpSurveys/Codility/StoneWall.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSurveys.Codility
+{
+    public static class StoneWall
+    {
+        public static int Solution(int[] H)
+        {
+            int stones = 0;
+            Stack<int> stoneStack = new Stack<int>();
+
+            for (int i = 0; i < H.Length; i++)
+            {
+                while ((stoneStack.Count > 0) && (stoneStack.Peek() > H[i]))
+                    stoneStack.Pop();
+
+                if ((stoneStack.Count > 0) && (stoneStack.Peek() == H[i]))
+                    continue;
+                else
+                {
+                    stones += 1;
+                    stoneStack.Push(H[i]);
+                }                
+            }
+            return stones;
+        }
+    }
+}
