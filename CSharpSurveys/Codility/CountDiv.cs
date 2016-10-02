b341e3044ae276c7a6617667135bfc4c940409ff
marcusvnac <marcusvnac@gmail.com>
2016-10-02 11:28:13 -0300

    CountDiv

--- CSharpSurveys/Codility/CountDiv.cs
+++ CSharpSurveys/Codility/CountDiv.cs
@@ -10,17 +10,29 @@ namespace CSharpSurveys
     {
         public static int Solution(int A, int B, int K)
         {
+            // Ninja solution
+            if (A % K == 0)
+                return ((B - A) / K) + 1;
+            else
+                return (B-(A- A%K))/ K;
+
+            /*
+             * // CS Guy Solution
             int count = 0;
+            
+            if (A < K)
+                return count;
+
             for (int i = A; i <= B; i++)
             {
                 if (i % K == 0)
                 {
-                    count++;
-                    i++;
+                    count++;            
                 }
             }
 
             return count;
+            */
         }
     }
 }
