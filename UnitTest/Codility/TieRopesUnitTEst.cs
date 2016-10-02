5b3fa2b7bdcdf20025d949bb2aef91199593c1d9
marcusvnac <marcusvnac@gmail.com>
2016-10-02 10:36:33 -0300

    TieRopes

---
+++ UnitTest/Codility/TieRopesUnitTEst.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys.Codility;
+
+namespace UnitTest.Codility
+{
+    [TestClass]
+    public class TieRopesUnitTest
+    {
+        [TestMethod]
+        public void TieRopesTestMethod1()
+        {
+            int[] A = { 1, 2, 3, 4, 1, 1, 3 };
+            int K = 4;
+
+            Assert.AreEqual(3, TieRopes.Solution(K, A));
+        } 
+    }
+}
