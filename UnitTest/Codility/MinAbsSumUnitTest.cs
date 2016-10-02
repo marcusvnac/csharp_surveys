6275fbf1ac16256ba36d56b5956ea33a4845862f
marcusvnac <marcusvnac@gmail.com>
2016-10-02 11:10:30 -0300

    MinAbsSum

---
+++ UnitTest/Codility/MinAbsSumUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpSurveys.Codility;
+
+namespace UnitTest.Codility
+{
+    [TestClass]
+    public class MinAbsSumUnitTest
+    {
+        [TestMethod]
+        public void MinAbsSumTestMethod1()
+        {
+            int[] A = { 1, 5, 2, -2 };
+
+            Assert.AreEqual(0, MinAbsSum.Solution(A));
+        }
+    }
+}
